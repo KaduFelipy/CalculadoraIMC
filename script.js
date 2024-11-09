6f93a2ff358edd325f7074fe1178e6351f7b4778
KaduFelipy <112670602+KaduFelipy@users.noreply.github.com>
2024-11-09 03:01:07 +0000

    SomeComment

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ botao.onclick = calcularIMC;
 
 function calcularIMC () {
     let imc = 0;
-
     let peso = parseInt(document.getElementById('peso').value);
     let altura = parseFloat(document.getElementById('altura').value);
     imc = peso/(altura^2);
@@ -25,7 +24,6 @@ function calcularIMC () {
         resultado.textContent = imc.toFixed(0);
         aviso.textContent = ' Cuidado! Você está abaixo do peso!';
         alertaSaude.textContent = 'Riscos á saúde = Transtornos digestivos, debilidade, fadiga crônica, stress, ansiedade e disfusões hormonais.';
-        
         alertaSaude.classList.add('orange');
 
     } if ( imc > 18 && imc < 25) { 
@@ -35,21 +33,19 @@ function calcularIMC () {
         alertaSaude.textContent = 'Riscos á saúde - Estado normal, bom nível de energia, vitalidade e boa condição física. Mantenha o seu peso e IMC.';
         alertaSaude.classList.add('green');
 
-    }  if (imc >= 25 && imc < 30) {
+    }   if (imc >= 25 && imc < 30) {
         textoResultado.textContent = 'Seu IMC é: ';
         resultado.textContent = imc.toFixed(0);
         aviso.textContent = ' Cuidado! Você está com sobrepeso!';
         alertaSaude.textContent = 'Risco moderado - Fadiga, problemas digestivos, problemas circulatórios, má circulação nas pernas, varizes.';
         alertaSaude.classList.add('yellow');
 
-    } if (imc >= 30 && imc < 35) {
+    }   if (imc >= 30 && imc < 35) {
         textoResultado.textContent = 'Seu IMC é: ';
         resultado.textContent = imc.toFixed(0);
         aviso.textContent = 'Cuidado! Você está com obesidade moderada!';
         alertaSaude.textContent = 'Risco alto - Diabetes, angina de peito, enfartes, tromboFlebites, arteroscleroses, alterações mestruais, disfunções sexuais.';
-        alertaSaude.classList.add('orange');
-
-    }if (imc >= 35) {
+    }   if (imc >= 35) {
         textoResultado.textContent = 'Seu IMC é: ';
         resultado.textContent = imc.toFixed(0);
         aviso.textContent = 'Cuidado! Você está com obesidade morbida!';
@@ -58,6 +54,3 @@ function calcularIMC () {
 
     }
 }
-
-
-
