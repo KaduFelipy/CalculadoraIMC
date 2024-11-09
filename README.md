fbf3a3c470d0522ea83066c4ff94c46b849b524e
KaduFelipy <112670602+KaduFelipy@users.noreply.github.com>
2024-11-09 01:35:19 +0000

    SomeComment

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CalculadoraIMC
-Calculadora de índice de massa corpórea
+Calculadora de índice de massa corpórea lecionada para o 2º ano do ensino técnico em desenvolvimento de sistemas - pela máteria de Front-End.
+
+Aprendizado:
+
+- Declaração de váriaveis
+- Uso de condicionais
+- Manipulação pelo DOM 
+- Uso de EventList
+- Hosting
--- script.js
+++ script.js
@@ -1,22 +1,63 @@
 const botao = document.getElementById('botao');
 const resultado = document.querySelector('.resultado');
-let imc = 0;
-
+const textoResultado = document.querySelector('.texto-resultado');
+const aviso = document.querySelector('.aviso');
+const alertaSaude = document.querySelector('.alertaSaude');
 
 
 botao.onclick = calcularIMC;
 
+
+
+
+
 function calcularIMC () {
-    let peso = document.getElementById('peso').value;
-    let altura = document.getElementById('altura').value;
-    
+    let imc = 0;
 
+    let peso = parseInt(document.getElementById('peso').value);
+    let altura = parseFloat(document.getElementById('altura').value);
     imc = peso/(altura^2);
-    resultado.textContent = imc.toFixed(0);
+
+    alertaSaude.classList.remove('yellow', 'red', 'orange', 'green');
+
+    if (imc <= 18) {
+        textoResultado.textContent = 'Seu IMC é: ';
+        resultado.textContent = imc.toFixed(0);
+        aviso.textContent = ' Cuidado! Você está abaixo do peso!';
+        alertaSaude.textContent = 'Riscos á saúde = Transtornos digestivos, debilidade, fadiga crônica, stress, ansiedade e disfusões hormonais.';
+        
+        alertaSaude.classList.add('orange');
+
+    } if ( imc > 18 && imc < 25) { 
+        textoResultado.textContent = 'Seu IMC é: ';
+        resultado.textContent = imc.toFixed(0);
+        aviso.textContent = ' Você está no peso Ideal!';
+        alertaSaude.textContent = 'Riscos á saúde - Estado normal, bom nível de energia, vitalidade e boa condição física. Mantenha o seu peso e IMC.';
+        alertaSaude.classList.add('green');
+
+    }  if (imc >= 25 && imc < 30) {
+        textoResultado.textContent = 'Seu IMC é: ';
+        resultado.textContent = imc.toFixed(0);
+        aviso.textContent = ' Cuidado! Você está com sobrepeso!';
+        alertaSaude.textContent = 'Risco moderado - Fadiga, problemas digestivos, problemas circulatórios, má circulação nas pernas, varizes.';
+        alertaSaude.classList.add('yellow');
+
+    } if (imc >= 30 && imc < 35) {
+        textoResultado.textContent = 'Seu IMC é: ';
+        resultado.textContent = imc.toFixed(0);
+        aviso.textContent = 'Cuidado! Você está com obesidade moderada!';
+        alertaSaude.textContent = 'Risco alto - Diabetes, angina de peito, enfartes, tromboFlebites, arteroscleroses, alterações mestruais, disfunções sexuais.';
+        alertaSaude.classList.add('orange');
+
+    }if (imc >= 35) {
+        textoResultado.textContent = 'Seu IMC é: ';
+        resultado.textContent = imc.toFixed(0);
+        aviso.textContent = 'Cuidado! Você está com obesidade morbida!';
+        alertaSaude.textContent = 'Risco Muito alto - Falta de Ar, apneia, sonolência, trombose pulmonar, úlceras varicosas, cancro do colon uterino e mamário, refluxo esofágico,disfunções sexuais, discriminação social, laboral e sexual.';
+        alertaSaude.classList.add('red');
+
+    }
 }
-
-
-
 
 
 
